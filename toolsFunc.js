e39d2fc620cf8c7badf1b37406ba0c21b9de871c
zhangxiaoleipy <zhangxiaolei@outlook.com>
2019-03-03 04:02:52 +0800

    添加随机函数
    先写进去，并没有用

--- toolsFunc.js
+++ toolsFunc.js
@@ -1,4 +1,100 @@
 
+/*
+//产生随机数的对象，功能参考Python。
+var random = {
+    //产生0-1之间的随机数
+    random: function () {
+        return Math.random();
+    },
+    //产生n - m之间的随机浮点数
+    uniform: function (begin, end) {
+        if (begin > end) {
+            [begin, end] = [end, begin];
+        }
+        return Math.random() * (end - begin) + begin;
+    },
+    //产生n - m之间的整数
+    randint: function (begin, end) {
+        if (begin > end) {
+            [begin, end] = [end, begin];
+        }
+        return parseInt(Math.random() * (end - begin + 1) + begin);
+    },
+    //从数组里随机选择一个数据
+    choice: function (arr) {
+        return arr[this.randint(0, arr.length - 1)];
+    },
+    //打乱数组里的元素排列顺序，返回的是一个新的数组
+    shuffle : function (arr) {
+        let index, tmp = [], len = arr.length;
+        for (let i = 0; i < len; i ++) {
+            index = this.randint(0, arr.length - 1);
+            tmp.push(arr[index]);
+            arr.splice(index, 1);
+        }
+        return tmp;
+    },
+
+    //获得一个递增的序列
+    getAscendingList: function (begin, end, step) {
+        let list = [];
+        for (let i = begin; i <= end; i += step) {
+            list.push(i);
+        }
+        return list;
+    },
+    //从递增序列里选择一个数据
+    randrange: function (begin, end, step) {
+        let list = this.getAscendingList(begin, end, step);
+        return this.choice(list);
+    },
+    //从数组里随机获得N个元素。
+    sample: function (arr, num) {
+        if (arr.length >= num) {
+            return this.shuffle(arr).slice(0, num);
+        } else {
+            console.error("数组长度小于随机数个数")
+        }
+    }
+};
+
+let tetrisRandCache = [0,0,0,0,0,0,0];
+let tetrisTmp = random.shuffle(random.getAscendingList(0, 6, 1));
+
+function rand() {
+
+    let t, index;
+
+    t = random.choice(tetrisTmp);
+  
+    index = tetrisTmp.indexOf(t);
+   
+    
+    if (tetrisRandCache[index] < 2) {
+        tetrisRandCache[index] += 1;
+    }
+
+    for (let i = 0; i < tetrisRandCache.length; i++) {
+        if (tetrisRandCache[i] >= 2) {
+            tetrisRandCache.splice(i, 1);
+            tetrisTmp.splice(i, 1);
+            break;
+        }
+    }
+
+    if (!tetrisTmp.length) {
+        tetrisTmp = random.shuffle(random.getAscendingList(0, 6, 1));
+        tetrisRandCache = [0,0,0,0,0,0,0];
+    }
+
+    return t + 1
+
+}
+
+*/
+
+
+
 
 //一个简单的定时器构造函数
 
